c0aa2920c912155c80d78597cedf640f9e0aa257
IhorRatsyn <ratsinjob@gmail.com>
2024-06-13 19:07:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
-const express = require('express');
 const initMongoConnection = require('./db/initMongoConnection');
+const setupServer = require('./server');
 
 const startServer = async () => {
   await initMongoConnection();
-
-  const app = express();
-
-  app.use(express.json());
-
-  app.get('/contacts', require('./routes/contacts'));
-
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  setupServer();
 };
 
 startServer();

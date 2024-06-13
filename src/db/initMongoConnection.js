25812785820d7ab6bd0ac344ae3791d4d9df7901
IhorRatsyn <ratsinjob@gmail.com>
2024-06-13 18:28:25 +0300

    виправлення 2

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,16 +3,17 @@ require('dotenv').config();
 
 const initMongoConnection = async () => {
   const connectionString = process.env.MONGODB_URL;
+  const databaseName = process.env.MONGODB_DB;
 
-  if (!connectionString) {
+  if (!connectionString || !databaseName) {
     console.error(
-      'MongoDB connection string is missing in environment variables.'
+      'MongoDB connection string or database name is missing in environment variables.'
     );
     process.exit(1);
   }
 
   try {
-    await mongoose.connect(connectionString);
+    await mongoose.connect(`${connectionString}/${databaseName}`);
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
